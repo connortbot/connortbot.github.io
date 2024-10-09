a7c0226ed7c800ee78641f4ee5851e3fd3290f3d
connortbot <loiconnor8@gmail.com>
2024-10-08 21:45:14 -0400

    experience to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,25 @@ import styled from 'styled-components';
 
 const HomeContainer = styled(motion.div)`
   width: 80%;
-  padding: 150px 80px;
   max-width: 1200px;
   margin: 0 auto;
   overflow: hidden;
   height: 80vh;
+  display: flex;
+  justify-content: space-between;
+  padding: 15vh 5vw;
+`;
+
+const LeftColumn = styled.div`
+  flex: 2;
+  padding-right: 40px;
+`;
+
+const RightColumn = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 `;
 
 const Greeting = styled.h1`
@@ -39,6 +53,34 @@ const WeakHighlight = styled.span`
   font-style: italic;
 `;
 
+const ExperienceItemContainer = styled.div`
+  margin: 10px 0;
+`;
+
+const ExperienceTitle = styled.p`
+  font-size: 18px;
+  color: #377047;
+  font-family: 'Hanken Grotesk', sans-serif;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const ExperienceCompany = styled.p`
+  font-size: 16px;
+  color: #377047;
+  font-family: 'Hanken Grotesk', sans-serif;
+  font-style: italic;
+  margin: 2px 0;
+`;
+
+const ExperienceDescription = styled.p`
+  font-size: 16px;
+  color: #6A6A6A;
+  font-family: 'Hanken Grotesk', sans-serif;
+  font-weight: 400;
+  margin: 2px 0;
+`;
+
 const pageVariants = {
     initial: {
       opacity: 0,
@@ -53,28 +95,48 @@ const pageVariants = {
     },
 };
 
+const ExperienceItem = ({ title, company, description }) => (
+  <ExperienceItemContainer>
+    <ExperienceTitle>{title}</ExperienceTitle>
+    <ExperienceCompany>{company}</ExperienceCompany>
+    <ExperienceDescription>{description}</ExperienceDescription>
+  </ExperienceItemContainer>
+);
 
 const Home = () => {
-    return (
-        <HomeContainer
-		initial="initial"
-		animate="animate"
-		exit="exit"
-		variants={pageVariants}
-		>
-            <Greeting>hi, i'm connor {'\u{1F631}'}</Greeting>
-            <Description>
-                {'\u{1F4BE}'} seeking 2025 internships at places i'll love, such as companies driven by film arts, machine learning, 
-                or something <WeakHighlight>interesting and meaningful.</WeakHighlight>
-            </Description>
-            <Description>
-                {'\u{1F4BC}'} prev. senior data <Highlight>@ cohere</Highlight>
-            </Description>
-            <Description>
-                {'\u{1F4D6}'} currently studying computer science {'\u{2795}'} business @ waterloo/laurier
-            </Description>
-        </HomeContainer>
-    );
+  return (
+    <HomeContainer
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={pageVariants}
+    >
+      <LeftColumn>
+        <Greeting>hi, i'm connor {'\u{1F631}'}</Greeting>
+        <Description>
+          {'\u{1F4BE}'} seeking 2025 internships at places i'll love, such as companies driven by film arts, machine learning, or something <WeakHighlight>interesting and meaningful.</WeakHighlight>
+        </Description>
+        <Description>
+          {'\u{1F4BC}'} prev. senior data <Highlight>@ cohere</Highlight>
+        </Description>
+        <Description>
+          {'\u{1F4D6}'} currently studying computer science {'\u{2795}'} business @ waterloo/laurier
+        </Description>
+      </LeftColumn>
+      <RightColumn>
+        <ExperienceItem
+          title="Software Engineer Intern (Senior Data Specialist)"
+          company="Cohere AI"
+          description="Retrieval Augmented Generation (RAG) for Command-R+"
+        />
+        <ExperienceItem
+          title="Software Engineer Intern"
+          company="Markland Wood Golf Club"
+          description="Cloud User Data Automation"
+        />
+      </RightColumn>
+    </HomeContainer>
+  );
 };
   
 export default Home;
