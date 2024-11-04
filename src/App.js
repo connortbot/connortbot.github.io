8d0d0aa68c4bdf56abc647c0d32316ead29cbdab
connortbot <loiconnor8@gmail.com>
2024-11-04 10:39:32 -0500

    v11 resume

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 						<AnimatedRoutes />
 					</ContentWrapper>
 					<Footer
-						resumeLink="https://drive.google.com/file/d/1Y-nDSO9mHEpWri3BsqUxIwAoTSxy9Bgu/view?usp=sharing"
+						resumeLink="https://drive.google.com/file/d/1ykxQm-1nh8dtcQqbI5sj8AAE43K54c9M/view?usp=sharing"
 						githubLink="https://github.com/connortbot"
 						linkedinLink="https://www.linkedin.com/in/connor-loi/"
 					/>
